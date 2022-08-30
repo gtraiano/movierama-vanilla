bfd4d873dd3b78ad98dd5cb9b94e77c496d0db18
gtraiano <gtraiano@gmail.com>
2022-08-30 23:24:25 +0300

    created new component alert-box
    used for displaying messages

---
+++ src/components/AlertBox/index.js
@@ -0,0 +1,51 @@
+import './style.css'
+
+const template = `<div class="alert-box"></div>`
+
+class AlertBox extends HTMLElement {
+    constructor() {
+        super()
+    }
+
+    connectedCallback() {
+        this.innerHTML = template
+    }
+
+    disconnectedCallback() {
+    }
+
+    static get observedAttributes() {
+        return []
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+    }
+
+    loading = (truth, message) => {
+        truth = Boolean(truth)
+        if(truth) {
+            this.setAttribute('show', truth.toString())
+            this.children[0].setAttribute('message', message ?? 'Fetching')
+            this.setAttribute('loading', truth.toString())
+        }
+        else {
+            this.removeAttribute('show')
+            this.children[0].removeAttribute('message')
+            this.removeAttribute('loading')
+        }
+    }
+
+    show = (truth, message) => {
+        truth = Boolean(truth)
+        if(truth) {
+            this.setAttribute('show', truth.toString())
+            message && this.children[0].setAttribute('message', message)
+        }
+        else {
+            this.removeAttribute('show')
+            this.children[0].removeAttribute('message')
+        }
+    }
+}
+
+customElements.define('alert-box', AlertBox)
