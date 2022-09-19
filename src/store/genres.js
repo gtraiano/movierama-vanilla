2acb9f0efc751d1d1f7d6d3e91269763bc7543a0
gtraiano <gtraiano@gmail.com>
2022-09-19 12:45:06 +0300

    sealed genres state slice object

--- src/store/genres.js
+++ src/store/genres.js
@@ -1,7 +1,7 @@
 // moviedb api genres
-export const genres = {
+export const genres = Object.seal({
     table: [],                  // genres lookup table
     lookup: function(id) {      // genres lookup function
         return this.table.find(g => g.id === id)?.name
     }
-}
+})
